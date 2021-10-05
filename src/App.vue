87cd3a3f51895eaf658d3273ca527a98c4f6cb5b
kintsyn <kintsyn@fix.ru>
2021-10-05 23:39:07 +0300

    example of using google map

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <div id="myMap"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }},
+  mounted: function() {
+    const map = new google.maps.Map(document.getElementById('myMap'), {
+      center: {lat:55.7829702, lng: 49.1059532},
+      scrollwheel: false,
+      zoom: 12
+    });
+
+    const infoWindow = new google.maps.InfoWindow();
+
+    const image =
+        "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+
+    let marker = new google.maps.Marker({
+      position: {lat:55.7829702, lng: 49.1059532},
+      map,
+      title: "Hello World!",
+      icon: image,
+      label: "keker"
+    });
+
+    const contentString = `
+        <div id="content">
+        <div id="siteNotice">
+        </div>
+        <h1 id="firstHeading" class="firstHeading">Пункт приема стеклотары</h1>
+        <div id="bodyContent">
+        <img src="https://recyclemap.ru/images/org_img/23191_20190904-220652_1.jpg"/>
+        <p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large
+        sandstone rock formation in the southern part of the
+        Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi)
+        south west of the nearest large town, Alice Springs; 450&#160;km
+        (280&#160;mi) by road. Kata Tjuta and Uluru are the two major
+        features of the Uluru - Kata Tjuta National Park. Uluru is
+        sacred to the Pitjantjatjara and Yankunytjatjara, the
+        Aboriginal people of the area. It has many springs, waterholes,
+        rock caves and ancient paintings. Uluru is listed as a World
+        Heritage Site.</p>
+        <p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">
+        https://en.wikipedia.org/w/index.php?title=Uluru</a>
+        (last visited June 22, 2009).</p>
+        </div>
+        </div>"
+    `;
+
+
+    marker.addListener("click", () => {
+      infoWindow.close();
+      infoWindow.setContent(contentString);
+      infoWindow.open(marker.getMap(), marker);
+    });
+
+
   }
+
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+#myMap {
+  height:600px;
+  width: 50%;
+  margin-left: auto;
 }
 </style>
